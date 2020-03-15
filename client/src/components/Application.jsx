5cdce41937676f547c53d14db9cc703f78901c22
SrdjanCoric <srdjan.coric1984@gmail.com>
2020-03-15 15:44:44 +0100

    ui templates added

--- client/src/components/Application.jsx
+++ client/src/components/Application.jsx
@@ -4,6 +4,17 @@ import TopNav from "./shared/TopNav";
 import BoardsDashboardContainer from "./dashboard/BoardsDashboardContainer";
 import BoardContainer from "./board/BoardContainer";
 import CardModalContainer from "./card/CardModalContainer";
+import UISection from "./ui/UISection";
+import AllBoards from "./ui/AllBoards";
+import CardArchived from "./ui/CardArchived";
+import CardEditingDescription from "./ui/CardEditingDescription";
+import Card from "./ui/Card";
+import CopyCardPopover from "./ui/CopyCardPopover";
+import CreateBoard from "./ui/CreateBoard";
+import DueDatePopover from "./ui/DueDatePopover";
+import LabelsPopover from "./ui/LabelsPopover";
+import MoveCardPopover from "./ui/MoveCardPopover";
+import SingleBoard from "./ui/SingleBoard";
 
 const Application = () => {
   return (
@@ -12,6 +23,20 @@ const Application = () => {
       <Route path="/" exact component={BoardsDashboardContainer} />
       <Route path="/(boards|cards)/:id" component={BoardContainer} />
       <Route path="/cards/:id" component={CardModalContainer} />
+      <Route path="/ui" exact component={UISection} />
+      <Route path="/ui/allBoards" component={AllBoards} />
+      <Route path="/ui/cardArchived" component={CardArchived} />
+      <Route
+        path="/ui/cardEditingDescription"
+        component={CardEditingDescription}
+      />
+      <Route path="/ui/card" component={Card} />
+      <Route path="/ui/copyCardPopover" component={CopyCardPopover} />
+      <Route path="/ui/createBoard" component={CreateBoard} />
+      <Route path="/ui/dueDatePopover" component={DueDatePopover} />
+      <Route path="/ui/labelsPopover" component={LabelsPopover} />
+      <Route path="/ui/moveCardPopover" component={MoveCardPopover} />
+      <Route path="/ui/singleBoard" component={SingleBoard} />
     </div>
   );
 };
